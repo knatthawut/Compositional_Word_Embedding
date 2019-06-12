92a2c3c419644616589c7d5493672ccdf8010aef
An DAO <daotuanan@gmail.com>
2019-06-12 11:18:18 +0900

    fix preprocessing

--- old/preprocessing.py
+++ old/preprocessing.py
@@ -4,6 +4,7 @@ Implementation of Preprocessing wikipedia data for training Word2Vec model
 
 import textacy
 import string
+import re
 
 input_file = ''
 output_file = ''
@@ -14,8 +15,36 @@ for c in string.punctuation:
     if c != {'_','/'}:
         punctuation_string += c
 
+def cleanLine(line):
+    pat = re.compile(r'[^\w-]')
+    replace = ' '    
+    clean = ''
+    items = line.split(' ')
+    if items[0].lower() == 'null':
+        items[0] = ''
+        if len(items) > 2 and items[2].lower() == 'redirect':
+            items[2] = ''
+    tmp = ''
+    for item in items:
+        if "DBPEDIA_ID" in item:
+            idx = item.find("DBPEDIA_ID")
+            if idx != 0:
+                item = item[:idx].lower()+' '+item[idx:]
+            tmp = pat.sub(replace,tmp)
+            clean = clean +' '+tmp+' '+item
+            tmp = ''
+        elif item != '':
+            if tmp != '':
+                tmp = tmp + ' '
+            tmp = tmp + item.lower()
+    tmp = pat.sub(replace,tmp)
+    clean = clean +' '+tmp
+    clean = clean.replace('  ',' ')
+    return clean.strip()
+
 def clean_text(text):
-    cleaned = textacy.preprocess.preprocess_text(text, fix_unicode=True, lowercase=False, no_urls=True, no_emails=True, no_phone_numbers=True, no_numbers=True, no_currency_symbols=True, no_contractions=True, no_accents=True)
+    cleaned = cleanLine(text)
+    cleaned = textacy.preprocess.preprocess_text(cleaned, fix_unicode=True, lowercase=False, no_urls=True, no_emails=True, no_phone_numbers=True, no_numbers=True, no_currency_symbols=True, no_contractions=True, no_accents=True)
     cleaned = textacy.preprocess.remove_punct(cleaned,marks=punctuation_string)
     cleaned = textacy.preprocess.normalize_whitespace(cleaned)
     return cleaned
@@ -27,3 +56,6 @@ def main():
             clean_line = clean_text(line)
             print(clean_line)
             break
+
+if __name__ == '__main__':
+    main()
