04822e1d15a938c7ced6b55e6218ca30a681a050
An DAO <daotuanan@gmail.com>
2019-06-14 14:59:16 +0900

    implement mark compound

--- old/mark_compound.py
+++ old/mark_compound.py
@@ -6,11 +6,11 @@ import re
 import spacy
 from spacy.tokenizer import Tokenizer
 
-prefix_re = re.compile(r'''DBPEDIA_ID\/''')
-infix_re = re.compile(r'''[-~\/\_\(\)]''')
+prefix_re = re.compile(r'''^DBPEDIA_ID\/[^\s]+''')
+# infix_re = re.compile(r'''[.\~\-\_\(\)]''')
 
 def custom_tokenizer(nlp):
-    return Tokenizer(nlp.vocab, prefix_search=prefix_re.search, infix_finditer=infix_re.finditer)
+    return Tokenizer(nlp.vocab, prefix_search=prefix_re.search)
 
 nlp = spacy.load("en_core_web_sm")
 nlp.tokenizer = custom_tokenizer(nlp)
@@ -68,6 +68,7 @@ def create_compound_dict(input_file):
 
     with open(input_file,'r', encoding = 'utf-8') as fin:
         for line in fin:
+            line = line.strip()
             tmp = line.split('\t')
             compound_id = tmp[0]
             element_words = tmp[1].split(' ')
@@ -94,35 +95,45 @@ def mark_on_text(wiki_input_file,output_file,compound_word_dict):
 
     # Open Wiki_input_file to write
     with open(wiki_input_file,'r',encoding = 'utf-8') as fin:
-        for line in fin:
+        for count, line in enumerate(fin):
             new_line = ''
+            check2word = False
             # Process 4 surface: 'dress code','dress codes','dresscode','dresscodes'
             # Remove _ and - without losing DBPEDIA_ID/compound_word
             doc = nlp(line)
             words = [t.text for t in doc]
             # get all unigram of the line
             for i in range(len(words)):
+                if check2word:
+                    check2word = False
+                    continue
+                # print('At step {} process {}'.format(str(i),words[i]))
                 # process unigram words[i]
                 if words[i] in compound_word_dict:
                     # meet dresscode or dresscodes
-                    new_line = new_line + compound_word_dict[words[i]]
-                elif (i < len(line)-1):
+                    # print('Compound Word: ', compound_word_dict[words[i]])
+                    new_line = new_line + ' ' + compound_word_dict[words[i]]
+                elif (i < (len(words)-1) ):
                     if words[i] + words[i+1] in compound_word_dict:
                         # Meet dress code or dress codes
-                        new_line = new_line + compound_word_dict[words[i]+words[i+1]]
-                else: new_line = new_line + words[i]
-            new_line += '\n'
+                        # print('Compound Word: ', compound_word_dict[words[i]])
+                        new_line = new_line + ' ' + compound_word_dict[words[i]+words[i+1]]
+                        check2word = True
+                    else: new_line = new_line + ' ' + words[i]
+                else: new_line = new_line + ' ' + words[i]
+                # print('New line: ',new_line)
+            new_line = new_line + '\n'
             fout.write(new_line)
 
 
 
-
 def main():
-    input_file = ''
-    wiki_input_file = ''
-    output_file = ''
+    compound_list_input = 'compound_word.tsv'
+    wiki_input_file = 'test'
+    output_file = 'test.out'
 
-    compound_word_dict = create_compound_dict(input_file)
+    compound_word_dict = create_compound_dict(compound_list_input)
+    # print(compound_word_dict)
     mark_on_text(wiki_input_file,output_file,compound_word_dict)
 
 if __name__ == '__main__':
