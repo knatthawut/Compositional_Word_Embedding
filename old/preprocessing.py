eb81bab534af5f4f68a45ca72b48aa2940ca7f79
An DAO <daotuanan@gmail.com>
2019-06-20 11:17:12 +0900

    fix bugs preprocessing

--- old/preprocessing.py
+++ old/preprocessing.py
@@ -42,8 +42,8 @@ def cleanLine(line):
 
 def clean_text(text):
     cleaned = cleanLine(text)
-    cleaned = textacy.preprocess.preprocess_text(cleaned, fix_unicode=True, lowercase=False, no_urls=True, no_emails=True, no_phone_numbers=True, no_numbers=True, no_currency_symbols=True, no_contractions=True, no_accents=True)
-    cleaned = textacy.preprocess.remove_punct(cleaned,marks=punctuation_string)
+    cleaned = textacy.preprocess.preprocess_text(cleaned, lowercase=False, no_urls=True, no_emails=True, no_phone_numbers=True, no_numbers=True, no_currency_symbols=True, no_contractions=True, no_accents=True)
+    # cleaned = textacy.preprocess.remove_punct(cleaned,marks=punctuation_string)
     cleaned = textacy.preprocess.normalize_whitespace(cleaned)
     return cleaned
 
