478e5f48b236a6cedaa93c2ff8ac14e47421b6f8
An DAO <daotuanan@gmail.com>
2019-06-21 18:55:47 +0900

    fix name of model

--- old/train_FastText.py
+++ old/train_FastText.py
@@ -25,13 +25,20 @@ if __name__ == "__main__":
 
 
     sentences = word2vec.LineSentence(sys.argv[1])
-    fname = sys.argv[2]
+    # fname = sys.argv[2]
     vec_size = int(sys.argv[3])
     sg_mark = int(sys.argv[4])
     wind = int(sys.argv[5])
     min_words = int(sys.argv[6])
     num_thread = int(sys.argv[7])
     model = FastText(sentences, size=vec_size, sg=sg_mark, window=wind, min_count=min_words, workers=num_thread)
+
+    if sg_mark == 1:
+        type_model = 'SG'
+    else:
+        type_model = 'CBOW'
+    
+    fname = sys.argv[2] + '{}_size{}_window{}_min{}'.format(type_model,vec_size,wind,num_thread)
 
     model.save(fname)
     print("Finished Saved Vector to " + fname)
--- old/train_WordVec.py
+++ old/train_WordVec.py
@@ -11,12 +11,18 @@ if __name__ == "__main__":
 
 
     sentences = word2vec.LineSentence(sys.argv[1])
-    fname = sys.argv[2]
     vec_size = int(sys.argv[3])
     sg_mark = int(sys.argv[4])
     wind = int(sys.argv[5])
     min_words = int(sys.argv[6])
     num_thread = int(sys.argv[7])
+    if sg_mark == 1:
+        type_model = 'SG'
+    else:
+        type_model = 'CBOW'
+    
+    fname = sys.argv[2] + '{}_size{}_window{}_min{}'.format(type_model,vec_size,wind,num_thread)
+
     model = word2vec.Word2Vec(sentences, size=vec_size, sg=sg_mark, window=wind, min_count=min_words, workers=num_thread)
 
     model.save(fname)
