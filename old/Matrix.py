30c89e3a77ebc6d6696ea76cd26472d0f47c5b64
An DAO <daotuanan@gmail.com>
2019-07-01 18:32:07 +0900

    try Matrix Baseline

---
+++ old/Matrix.py
@@ -0,0 +1,56 @@
+#Import Libraries
+import tensorflow as tf
+from keras.layers import SimpleRNN, Embedding
+from keras.models import Sequential
+from keras.initializers import Constant
+from gensim.models import Word2Vec
+import functools
+import numpy as np
+import sys
+import os
+import pprint
+from keras.preprocessing.text import Tokenizer
+pp = pprint.PrettyPrinter(indent=4)
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import StratifiedKFold
+from Keras_baseline import KERAS_baseline
+# ***************
+# Constant Declaration
+# ***************
+
+
+
+
+# ***************
+# Model Definitions
+# ***************
+
+# Baseline: Simple RNN network without attention
+class Matrix_baseline(KERAS_baseline):
+    '''
+    Class for Simple RNN baseline
+    '''
+    def __init__(self, type_of_wordvec, vocab_size, embedding_dim,
+                 embedding_matrix, MAX_SEQUENCE_LENGTH=21,type_of_loss_func ='mean_squared_error', type_of_optimizer = 'adam',
+                 activation_func = 'tanh'):
+        super().__init__('Matrix', type_of_wordvec, vocab_size, embedding_dim, embedding_matrix, MAX_SEQUENCE_LENGTH, type_of_loss_func=type_of_loss_func, type_of_optimizer=type_of_optimizer)
+        self.activation_func = activation_func
+        # self.print_information()
+        # Model Definition of Simple RNN network
+        self.model =  Sequential() # Define Sequential Model
+        embedding_layer = Embedding(self.vocab_size,
+                                self.embedding_dim*2,
+                                weights=[embedding_matrix],
+                                input_length=self.MAX_SEQUENCE_LENGTH,
+                                trainable=False)
+        self.model.add(embedding_layer) # Add the Embedding layers to the model
+        self.model.add(SimpleRNN(self.embedding_dim))
+        # Print Model Summary to see the architecture of model
+        print(self.model.summary())
+
+
+    def print_information(self):
+        super().print_information()
+        # print('Attention Activation: ',self.attention_activation)
+        print('Activation Function: ',self.activation_func)
+        print(self.model.summary())
