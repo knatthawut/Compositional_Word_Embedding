647d9e9278735236194032a12a429eba6a022d5e
An DAO <daotuanan@gmail.com>
2019-07-08 20:47:02 +0900

    Implementing Extract text from the Encow corpus

--- old/extract_text_encow.py
+++ old/extract_text_encow.py
@@ -4,23 +4,65 @@
 '''
 # Import Libraries
 import sys
+import re
 
 
 # Import modules
 
+# Constants
+sent_begin_regex = re.compile(r'<s\sxid=.*>') 
 
 # Function Implementations
+def is_sent_start(line):
+    '''
+    Check if that line is the start of sentence
+    '''
+    match = re.match(sent_begin_regex,line)
+    if match:
+        return True
+    else:
+        return False
+
+def is_sent_end(line):
+    return (line == '</s>')
+
+def get_text(sent):
+    '''
+    Convert sent (list of words)
+    '''
+    res = ''
+    for token in sent:
+        if is_tag(token):
+            if is_nc_start(token):
+                
+            elif is_nc_end(token):
+        else: # Normal word
+
+    return res
 
 def main(input_file,output_file):
     # Open Input file and read each line
     fout = open(output_file,'w',encoding='utf-8')
+    sent = []
+    in_sent = False
     with open(input_file,'r',encoding='utf-8') as fin:
-        for i, line in enumerate(fin):
-            print('Processing {} line: {}'.format(i,line))
+        for i, tmp in enumerate(fin):
+            line = tmp.split('\t')[0]
+            # print('Sent: ',sent)
+            # print('Processing {} line: {}'.format(i,line))
+            if is_sent_start(line):
+                # print('End of Sent!!!!!')
+                print('Sent ',sent)
+                raw_text = get_text(sent[:-1])
+                fout.write(raw_text+'\n')
+                
+                sent = []
+            else:
+                sent.append(line.strip())
             
             if (i>200):
                 break
-
+    fout.close()
 
 
 if __name__ == '__main__':
