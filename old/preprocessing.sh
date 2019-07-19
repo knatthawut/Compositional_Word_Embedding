cc064d21473b6463990258d1e6fd5bd95c7f3ff3
An DAO <daotuanan@gmail.com>
2019-07-19 12:01:43 +0900

    change preprocessing.sh

--- old/preprocessing.sh
+++ old/preprocessing.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo -n "Input the path to the input_file: "
-read Input_file
-echo -n "Input the path to the output_file: "
-read Out_file
+Input_file=''
+Out_file=''
 Tmp_file='process.tmp'
 python multi_file_processing.py $Input_file $Tmp_file
 python generate_word_vector_train_data.py $Tmp_file $Out_file
