8c144cf4528793fca82d42721b4e83104cf2a416
An DAO <andao@Ans-MacBook-Air.mshome.net>
2020-04-05 17:20:24 +0900

    update convert analogy into compounds

---
+++ old/analogy_to_compound.py
@@ -0,0 +1,29 @@
+'''
+Convert Analogy test into Compounds.
+
+'''
+import argparse
+
+def process(input_file,output_file):
+    fout = open(output_file,'w',encoding='utf-8')
+
+    with open(input_file,'r',encoding='utf-8') as fin:
+        for line in fin:
+            line = line.strip()
+            phrases = line.split('\t')
+            for phrase in phrases:
+                elements = phrases.split('_')
+                compound = 'COMPOUND_ID/' + phrase + '\t' + ' '.join(elements) + '\n'
+                fout.write(compound)
+    fout.close()
+
+
+if __name__ == '__main__':
+    # Parse the input arguments
+    parser = argparse.ArgumentParser(description='Extract Compound from Phrase Analogy Test')
+    parser.add_argument('--input',type=str, metavar='', required=True, help='data input file')
+    parser.add_argument('--output',type=str, metavar='', required=True, help='data out file')
+    args = parser.parse_args()
+    input_file = args.input
+    output_file = args.output
+    process(input_file,output_file)
